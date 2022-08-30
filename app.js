7453eed0cbcd60120d6155c1446e9a2180b0de13
nina <nina10313@gmail.com>
2022-08-30 23:49:57 +0800

    feat: 設定server及設定不同請求的回應 新增項目: 1.建立伺服器路徑為http://localhost:3000/ 2.設定3種請求(首頁,cats,dogs)的回應

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("You are on the homepage.");
+});
+
+app.get("/cats", (req, res) => {
+  res.send("This page is about cats.");
+});
+
+app.get("/dogs", (req, res) => {
+  res.send("This page is about dogs.");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000.");
+});
