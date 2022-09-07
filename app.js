368929b3759426d936b53d4c8603fac03f87e483
nina <nina10313@gmail.com>
2022-09-07 18:00:19 +0800

    feat: 練習 routing for query (post method)

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("This is homepage.");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/cats", (req, res) => {
@@ -19,6 +22,14 @@ app.get("/animal/:typeOfAnimal", (req, res) => {
 
 app.get("*", (req, res) => {
   res.send("Can't find the page.");
+});
+
+app.post("/formHandling", (req, res) => {
+  let { fullname, age } = req.body;
+  res.send(
+    `Thanks for posting. 
+    Your name is ${fullname} and your age is ${age}.`
+  );
 });
 
 app.listen(3000, () => {
